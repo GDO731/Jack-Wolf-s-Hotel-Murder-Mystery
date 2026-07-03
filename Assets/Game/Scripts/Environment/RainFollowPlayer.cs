56b602f1a0586962cecde3fcc5e1e39ab0be75c2
HSTV-FAL <qn336184@falmouth.ac.uk>
2026-07-02 19:05:00 -0600

    Added Rain Sounds and Dynamic Levels
    Added Audio from 99sounds.org free of charge. I created a small organized script that checks if a player collider and a layer collider interact. If so it will switch between audio Snapshot layers decreasing the decibel level.

--- Assets/Game/Scripts/Environment/RainFollowPlayer.cs
+++ Assets/Game/Scripts/Environment/RainFollowPlayer.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class RainFollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset = new Vector3(0, 20, 0); // Location of Rain Above Player
+    // Location of the Rain Particle above the player
+    public Vector3 offset = new Vector3(0, 20, 0);
 
     void Update()
     {
         if (player != null)
         {
+            // Update Rain Particle to follow player position plus its offset
             transform.position = player.position + offset;
         }
     }
